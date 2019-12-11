981b13aa192bd86630c46a5aaf82ec3fe6c66a7a
Irina Lukina <ibenderina@gmail.com>
2019-12-11 14:39:31 +0300

    Пиксель перфект главная

--- source/js/script.js
+++ source/js/script.js
@@ -24,18 +24,41 @@ navToggle.addEventListener('click', function() {
   }
 });
 
-var catBefore = document.querySelector('.results__photo-item--before');
-var catAfter = document.querySelector('.results__photo-item--after');
-var buttonBefore = document.querySelector('.results__range-button--before');
-var buttonAfter = document.querySelector('.results__range-button--after');
-var commonRange = document.querySelector('.results__range-scale');
+var toggleBefore = document.querySelector('.slider__toggle--before');
+var photoBefore = document.querySelector('.slider__photo--before');
+var toggleAfter = document.querySelector('.slider__toggle--after');
+var photoAfter = document.querySelector('.slider__photo--after');
+var slider = document.querySelector('.slider__button');
+var container = document.querySelector('.slider__container');
 
-commonRange.addEventListener ('click', function() {
-  if (catAfter.classList.contains('results__photo-item--closed')) {
-    catAfter.classList.remove('results__photo-item--closed');
-    catBefore.classList.add('results__photo-item--closed');
-  } else {
-    catBefore.classList.remove('results__photo-item--closed');
-    catAfter.classList.add('results__photo-item--closed');
+toggleAfter.addEventListener('click', function() {
+  if (photoAfter.classList.contains('results__photo-item--closed')) {
+    photoAfter.classList.remove('results__photo-item--closed');
+    photoBefore.classList.add('results__photo-item--closed');
+    slider.classList.add('slider__button--after');
+    container.classList.remove('slider__container--before');
+    container.classList.add('slider__container--after');
+
+    if (document.documentElement.clientWidth >= 768) {
+      slider.classList.remove('slider__button--before');
+      slider.classList.add('slider__button--after');
+      photoAfter.classList.add('slider__photo-item--active');
+    }
+  }
+});
+
+toggleBefore.addEventListener('click', function() {
+  if (photoBefore.classList.contains('results__photo-item--closed')
+    && slider.classList.contains('slider__button--after')) {
+    photoBefore.classList.remove('results__photo-item--closed');
+    photoAfter.classList.add('results__photo-item--closed');
+    slider.classList.remove('slider__button--after');
+    container.classList.remove('slider__button--after');
+    container.classList.add('slider__button--before');
+
+    if (document.documentElement.clientWidth >= 768) {
+      slider.classList.add('slider__button--before');
+      photoBefore.classList.add('slider__photo-item--active');
+    }
   }
 });
